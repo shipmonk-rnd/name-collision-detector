af40aa46686485738f35d884772cbf1e727725c9
Jan Nedbal <janedbal@gmail.com>
2022-12-22 13:52:17 +0100

    Add test for binary

--- tests/NameCollisionDetectorTest.php
+++ tests/NameCollisionDetectorTest.php
@@ -3,9 +3,51 @@
 namespace ShipMonk;
 
 use PHPUnit\Framework\TestCase;
+use function fclose;
+use function proc_close;
+use function proc_open;
+use function stream_get_contents;
 
 class NameCollisionDetectorTest extends TestCase
 {
+
+    public function testBinScript(): void
+    {
+        $expectedNoDirectory = "ERROR: no directories provided, use e.g. `detect-collisions src tests`\n";
+        $expectedInvalidDirectoryRegex = "~^ERROR: \".*?/tests/nonsense\" does not exist\n$~";
+        $expectedSuccessRegex = <<<'EOF'
+~Checking duplicates of classes and functions and constants in ../src:
+
+OK: no name collision found in: .*?/src~
+EOF;
+        $space = ' '; // bypass editorconfig checker
+        $expectedClasses = <<<EOF
+Checking duplicates of classes in sample-collisions:
+
+Foo\NamespacedClass1 is defined 2 times:
+$space> /sample-collisions/file1.php
+$space> /sample-collisions/file2.php
+
+Foo\NamespacedClass2 is defined 2 times:
+$space> /sample-collisions/file2.php
+$space> /sample-collisions/file2.php
+
+GlobalClass1 is defined 2 times:
+$space> /sample-collisions/file1.php
+$space> /sample-collisions/file2.php
+
+GlobalClass2 is defined 2 times:
+$space> /sample-collisions/file2.php
+$space> /sample-collisions/file2.php
+
+
+EOF;
+
+        self::assertSame($expectedClasses, $this->runCommand(__DIR__ . '/../bin/detect-collisions --classes sample-collisions', 1));
+        self::assertSame($expectedNoDirectory, $this->runCommand(__DIR__ . '/../bin/detect-collisions', 255));
+        self::assertMatchesRegularExpression($expectedInvalidDirectoryRegex, $this->runCommand(__DIR__ . '/../bin/detect-collisions nonsense', 255));
+        self::assertMatchesRegularExpression($expectedSuccessRegex, $this->runCommand(__DIR__ . '/../bin/detect-collisions ../src', 0));
+    }
 
     public function testCollisionDetection(): void
     {
@@ -63,6 +105,29 @@ class NameCollisionDetectorTest extends TestCase
             ],
             $collidingConstants
         );
+    }
+
+    private function runCommand(string $command, int $expectedExitCode): string
+    {
+        $desc = [
+            ['pipe', 'r'],
+            ['pipe', 'w'],
+        ];
+
+        $cwd = __DIR__;
+        $procHandle = proc_open($command, $desc, $pipes, $cwd);
+        self::assertNotFalse($procHandle);
+
+        $output = stream_get_contents($pipes[1]);
+        self::assertNotFalse($output);
+
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+
+        $exitCode = proc_close($procHandle);
+        self::assertSame($expectedExitCode, $exitCode);
+
+        return $output;
     }
 
 }
