645080235688d1a2a094612bb9a0819e8d31af42
Jan Nedbal <janedbal@gmail.com>
2023-08-25 11:52:38 +0200

    Try bypass shell

--- tests/CollisionDetectorTest.php
+++ tests/CollisionDetectorTest.php
@@ -103,7 +103,7 @@ EOF;
         ];
 
         $cwd = __DIR__;
-        $procHandle = proc_open($command, $desc, $pipes, $cwd);
+        $procHandle = proc_open($command, $desc, $pipes, $cwd, null, ['bypass_shell' => true]);
         self::assertNotFalse($procHandle);
 
         $output = stream_get_contents($pipes[1]);
